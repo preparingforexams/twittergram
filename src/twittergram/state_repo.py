fbad5ee65847e0a8f89b9b4f9f06c7db6463ed78
Björn Petersen <git@bjoernpetersen.net>
2022-09-19 00:03:27 +0200

    Fix aiofiles usage

--- src/twittergram/state_repo.py
+++ src/twittergram/state_repo.py
@@ -32,11 +32,11 @@ class FileStateRepo(StateRepo):
             return None
 
         async with aio_open(self.path) as f:
-            content = f.read()
+            content = await f.read()
             data = json.loads(content)
             return State(**data)
 
     async def store_state(self, state: State):
         async with aio_open(self.path, "w") as f:
             content = json.dumps(dataclasses.asdict(state))
-            f.write(content)
+            await f.write(content)
