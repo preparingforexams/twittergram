47f3d5a616f92bdacb7d8b51cde1f0120a94762b
Björn Petersen <git@bjoernpetersen.net>
2023-10-20 12:57:04 +0200

    Finish reddit forwarding

--- src/twittergram/application/use_cases/forward_reddit_posts.py
+++ src/twittergram/application/use_cases/forward_reddit_posts.py
@@ -5,7 +5,7 @@ from injector import inject
 
 from twittergram.application import ports, repos
 from twittergram.config import RedditConfig
-from twittergram.domain.model import RedditPost, RedditState
+from twittergram.domain.model import MediaType, Medium, RedditPost, RedditState
 
 _LOG = logging.getLogger(__name__)
 
@@ -14,6 +14,7 @@ _LOG = logging.getLogger(__name__)
 @dataclass
 class ForwardRedditPosts:
     config: RedditConfig
+    media_downloader: ports.MediaDownloader
     reddit_reader: ports.RedditReader
     state_repo: repos.StateRepo
     telegram_uploader: ports.TelegramUploader
@@ -30,15 +31,36 @@ class ForwardRedditPosts:
             if subreddit_filter is not None and post.subreddit_name == subreddit_filter:
                 posts.append(post)
 
-            if last_post_time is None or post.created_at <= last_post_time:
+            if last_post_time is None and len(posts) == 5:
                 break
+
+            if last_post_time is not None and post.created_at <= last_post_time:
+                break
+
+        if not posts:
+            _LOG.info("No posts found")
+            return
 
         # We want to forward post them in reverse order
         posts.reverse()
 
+        _LOG.info("Downloading media")
+        media = [
+            Medium(type=MediaType.PHOTO, id=post.id, url=str(post.url))
+            for post in posts
+        ]
+        media_files = await self.media_downloader.download(media)
+
+        media_file_by_post_id = {file.medium.id: file for file in media_files}
+
+        _LOG.info("Forwarding %d posts", len(posts))
         try:
             for post in posts:
-                _LOG.info("Would forward %s", post.title)
-
+                await self.telegram_uploader.send_image_message(
+                    image_files=[media_file_by_post_id[post.id]],
+                    caption=post.title,
+                )
+                state.last_post_time = post.created_at
         finally:
+            _LOG.debug("Storing state")
             await self.state_repo.store_state(state)
