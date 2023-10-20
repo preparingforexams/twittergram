78be7e798440a2d6cd6d1572ce671deec8706cda
Björn Petersen <git@bjoernpetersen.net>
2023-10-20 11:46:47 +0200

    Use asyncpraw

--- src/twittergram/infrastructure/adapters/reddit_reader/praw.py
+++ src/twittergram/infrastructure/adapters/reddit_reader/praw.py
@@ -1,4 +1,4 @@
-import praw
+import asyncpraw
 
 from twittergram.application.ports import RedditReader
 from twittergram.config import RedditConfig
@@ -6,12 +6,18 @@ from twittergram.config import RedditConfig
 
 class PrawRedditReader(RedditReader):
     def __init__(self, config: RedditConfig) -> None:
-        self.reddit = praw.Reddit(
+        self.config = config
+
+    @property
+    def reddit(self) -> asyncpraw.Reddit:
+        config = self.config
+        return asyncpraw.Reddit(
             client_id=config.client_id,
             client_secret=config.client_secret,
             user_agent=config.user_agent,
         )
 
     async def lookup_user_id(self, name: str) -> str:
-        redditor = self.reddit.redditor(name)
-        return redditor.id
+        async with self.reddit as reddit:
+            redditor = await reddit.redditor(name, fetch=True)
+            return redditor.id
