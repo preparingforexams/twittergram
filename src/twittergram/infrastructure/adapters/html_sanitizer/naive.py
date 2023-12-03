9028c7f768dcc94ee81d8e5a184b27f658b736c0
Björn Petersen <git@bjoernpetersen.net>
2023-12-03 12:15:38 +0100

    Properly replace spans

--- src/twittergram/infrastructure/adapters/html_sanitizer/naive.py
+++ src/twittergram/infrastructure/adapters/html_sanitizer/naive.py
@@ -1,13 +1,18 @@
+import re
+
 from twittergram.application.ports import HtmlSanitizer
 
 
 class NaiveHtmlSanitizer(HtmlSanitizer):
+    SPAN = re.compile(r"<span[^>]*>")
+
     async def sanitize(self, raw: str) -> str:
-        return (
+        simple_sanitized = (
             raw.replace("<br />", "\n")
             .replace("<p>", "\n\n")
             .replace("</p>", "")
-            .replace("<span>", "")
             .replace("</span>", "")
             .strip()
         )
+
+        return self.SPAN.sub("", simple_sanitized)
