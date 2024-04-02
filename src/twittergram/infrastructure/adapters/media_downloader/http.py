125db304e8b4e08602869a8aac41ce42551512c8
Björn Petersen <git@bjoernpetersen.net>
2024-04-02 23:39:43 +0200

    Use httpx in media_downloader

--- src/twittergram/infrastructure/adapters/media_downloader/http.py
+++ src/twittergram/infrastructure/adapters/media_downloader/http.py
@@ -1,13 +1,11 @@
 import logging
 import mimetypes
-import ssl
 import uuid
 from pathlib import Path
 
 import aiofiles
-import aiohttp
-import certifi
 from aiofiles.os import makedirs
+from httpx import AsyncClient
 
 from twittergram.application.exceptions.io import IoException
 from twittergram.application.exceptions.media import UnsupportedMediaTypeException
@@ -23,10 +21,10 @@ class HttpMediaDownloader(MediaDownloader):
         self.directory = directory
 
     @staticmethod
-    async def _session() -> aiohttp.ClientSession:
-        ssl_context = ssl.create_default_context(cafile=certifi.where())
-        connector = aiohttp.TCPConnector(ssl=ssl_context)
-        return aiohttp.ClientSession(connector=connector)
+    async def _session() -> AsyncClient:
+        return AsyncClient(
+            timeout=60,
+        )
 
     async def download(self, media: list[Medium]) -> list[MediaFile]:
         for medium in media:
@@ -42,33 +40,33 @@ class HttpMediaDownloader(MediaDownloader):
         async with await self._session() as session:
             # TODO: we could utilize asyncio more here, but meh
             for medium in media:
-                async with session.get(medium.url) as response:
-                    match response.status:
-                        case 200:
-                            mime_type = response.content_type
-                            extension = mimetypes.guess_extension(mime_type) or ""
-                            path = directory / f"{medium.id}.{extension}"
+                response = await session.get(medium.url)
+                match response.status_code:
+                    case 200:
+                        mime_type = response.headers.get("Content-Type")
+                        extension = mimetypes.guess_extension(mime_type) or ""
+                        path = directory / f"{medium.id}.{extension}"
 
-                            async with aiofiles.open(path, "wb") as f:
-                                await f.write(await response.read())
+                        async with aiofiles.open(path, "wb") as f:
+                            await f.write(response.content)
 
-                            media_file = MediaFile(
-                                medium=medium,
-                                path=path,
-                                mime_type=mime_type,
-                            )
-                            result.append(media_file)
-                        case status_code if 400 <= status_code < 500:
-                            _LOG.error(
-                                "Received status code %d for URL %s. Skipping.",
-                                response.status,
-                                medium.url,
-                            )
-                        case status_code if 500 <= status_code < 600:
-                            raise IoException(
-                                "Received server error %d for URL %s",
-                                status_code,
-                                medium.url,
-                            )
+                        media_file = MediaFile(
+                            medium=medium,
+                            path=path,
+                            mime_type=mime_type,
+                        )
+                        result.append(media_file)
+                    case status_code if 400 <= status_code < 500:
+                        _LOG.error(
+                            "Received status code %d for URL %s. Skipping.",
+                            status_code,
+                            medium.url,
+                        )
+                    case status_code if 500 <= status_code < 600:
+                        raise IoException(
+                            "Received server error %d for URL %s",
+                            status_code,
+                            medium.url,
+                        )
 
         return result
