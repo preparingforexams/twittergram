b2ca936da0595620c22cc47209496f2b15929f15
Björn Petersen <git@bjoernpetersen.net>
2025-10-04 12:26:09 +0200

    Implement Message tag in sanitizer
    Someday I'll rewrite a proper sanitizer

--- src/twittergram/infrastructure/adapters/html_sanitizer/naive.py
+++ src/twittergram/infrastructure/adapters/html_sanitizer/naive.py
@@ -1,11 +1,34 @@
+import logging
 import re
 
 from twittergram.application.ports import HtmlSanitizer
+
+_LOG = logging.getLogger(__name__)
 
 
 class NaiveHtmlSanitizer(HtmlSanitizer):
     IMG = re.compile(r"<img[^>]*>")
     SPAN = re.compile(r"<span[^>]*>")
+    MESSAGE = re.compile(r"<[Mm]essage( type=\"(?P<type>\w+)\")?>")
+
+    @staticmethod
+    def _choose_emoji(message_type: str | None) -> str:
+        match message_type:
+            case "important":
+                return "⚠️"
+            case "note":
+                return "ℹ️"
+            case None:
+                return ""
+            case other:
+                _LOG.error("Unknown HTML message type: %s", other)
+                return ""
+
+    def _replace_message(self, match: re.Match[str]) -> str:
+        message_type = match.group("type")
+
+        emoji = self._choose_emoji(message_type)
+        return f"\n{emoji}: "
 
     async def sanitize(self, raw: str) -> str:
         simple_sanitized = (
@@ -23,9 +46,14 @@ class NaiveHtmlSanitizer(HtmlSanitizer):
             .replace("</h2>", "</b>")
             .replace("<h3>", "<b>")
             .replace("</h3>", "</b>")
+            .replace("</Message>", "\n")
         )
 
         simple_sanitized = self.IMG.sub("", simple_sanitized)
         simple_sanitized = self.SPAN.sub("", simple_sanitized)
+        simple_sanitized = self.MESSAGE.sub(
+            repl=self._replace_message,
+            string=simple_sanitized,
+        )
 
         return simple_sanitized.strip()
